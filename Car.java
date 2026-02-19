0fba515c77998861ab99e88cd8ed7c6888970005
Olivia Waithira <oliviawaithira7@gmail.com>
2026-02-19 23:22:12 +0300

    details about car

--- Car.java
+++ Car.java
@@ -1,4 +1,17 @@
 // Car.java
 public class Car extends Vehicle {
-    
+      // Additional field
+    private String fuelType;
+
+    // Constructor
+    public Car(String brand, String model, int year, String fuelType) {
+        super(brand, model, year);  // Call parent constructor
+        this.fuelType = fuelType;
+    }
+
+    // Overriding displayDetails method
+    @Override
+    public void displayDetails() {
+        super.displayDetails();  // Call parent method
+        System.out.println("Fuel Type: " + fuelType);
 }
